48507ebc48a53cb42cda0f3f0b78d602b165f70c
junah201 <junah.dev@gmail.com>
2025-03-25 18:13:49 +0900

    Feat: Initial setup for nexify and add solve api

---
+++ backend/main.py
@@ -0,0 +1,44 @@
+import json
+from typing import Annotated
+import os
+from nexify import Body, Nexify
+from nexify.responses import JSONResponse
+from pydantic import BaseModel, Field
+
+
+def cors_middleware(route, event, context, call_next):
+    print("cors_middleware start")
+    response = call_next(event, context)
+    print("cors_middleware end")
+    response.headers["Access-Control-Allow-Origin"] = "https://decompiler-25spring-door-quiz.junah.dev"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
+
+
+app = Nexify(middlewares=[cors_middleware])
+
+flag1 = os.environ.get('FLAG1', "1")
+flag2 = os.environ.get('FLAG2', "2")
+DOOR_PASSWORD = os.environ.get('DOOR_PASSWORD')
+
+
+class Flags(BaseModel):
+    flag1: Annotated[str, Field(min_length=2, max_length=512)]
+    flag2: Annotated[str, Field(min_length=2, max_length=512)]
+
+
+class SolveResponse(BaseModel):
+    message: str
+
+
+@app.post("/solve")
+def solve(flags: Annotated[Flags, Body()]):
+    print("main functions")
+    if flags.flag1 != flag1 or flags.flag2 != flag2:
+        return JSONResponse(
+            status_code=400,
+            content={"detail": "Invalid flags"}
+        )
+
+    return SolveResponse(password=DOOR_PASSWORD)
